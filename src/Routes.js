e7adf487d92a7db401c38798a001561798d9d3e4
redwoodcodeacademy <info@redwoodcodeacademy.com>
2021-01-15 10:59:41 -0800

    Building our React app

--- src/Routes.js
+++ src/Routes.js
@@ -1,7 +1,14 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
+import AuthenticatedRoute from "./components/AuthenticatedRoute";
+import UnauthenticatedRoute from "./components/UnauthenticatedRoute";
 import Home from "./containers/Home";
+import Login from "./containers/Login";
+import NewNote from "./containers/NewNote";
+import Notes from "./containers/Notes";
 import NotFound from "./containers/NotFound";
+import Settings from "./containers/Settings";
+import Signup from "./containers/Signup";
 
 export default function Routes() {
     return (
@@ -9,6 +16,21 @@ export default function Routes() {
             <Route exact path="/">
                 <Home />
             </Route>
+            <UnauthenticatedRoute exact path="/login">
+                <Login />
+            </UnauthenticatedRoute>
+            <UnauthenticatedRoute exact path="/signup">
+                <Signup />
+            </UnauthenticatedRoute>
+            <AuthenticatedRoute exact path="/settings">
+                <Settings />
+            </AuthenticatedRoute>
+            <AuthenticatedRoute exact path="/notes/new">
+                <NewNote />
+            </AuthenticatedRoute>
+            <AuthenticatedRoute exact path="/notes/:id">
+                <Notes />
+            </AuthenticatedRoute>
             {/* Finally, catch all unmatched routes */}
             <Route>
                 <NotFound />
--- src/config.js
+++ src/config.js
@@ -1,18 +1,20 @@
 const config = {
-    s3: {
-      REGION: "us-east-1",
-      BUCKET: "paul-epps-notes-app-uploads",
-    },
-    apiGateway: {
-      REGION: "us-east-1",
-      URL: "https://k2wyntb5t7.execute-api.us-east-1.amazonaws.com/prod",
-    },
-    cognito: {
-      REGION: "us-east-1",
-      USER_POOL_ID: "us-east-1_rBIfCI7S9",
-      APP_CLIENT_ID: "1thq93f0s6l3jnbs0utuimp7vc",
-      IDENTITY_POOL_ID: "us-east-1:c5a22766-1d4d-405f-9baf-7036ad49c324",
-    },
-  };
-  
-  export default config;
+  STRIPE_KEY: "pk_test_51H2QNlJqdiwdYo5w80EVPQmV0uZRSuzMhDDriU75shXvF5l9swuJZOxpdGOHBeBtoNDFerTvxSzhQZPM43KDP9gD00yt5S8DH5",
+  MAX_ATTACHMENT_SIZE: 5000000,
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "paul-epps-notes-app-upload",
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://k2wyntb5t7.execute-api.us-east-1.amazonaws.com/prod",
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_rBIfCI7S9",
+    APP_CLIENT_ID: "1thq93f0s6l3jnbs0utuimp7vc",
+    IDENTITY_POOL_ID: "us-east-1:c5a22766-1d4d-405f-9baf-7036ad49c324",
+  },
+};
+
+export default config;
